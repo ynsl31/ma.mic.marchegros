8f01472405b257ef212f4cb2c4fea643e6ea86e8
ynsl31 <ynsln1998@gmail.com>
2020-08-12 11:07:20 +0100

    masterpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './master/footer/footer.component';
+import { NavbarComponent } from './master/navbar/navbar.component';
+import { SidebarComponent } from './master/sidebar/sidebar.component';
+import { HeaderContentComponent } from './master/header-content/header-content.component';
+import { ContentComponent } from './master/content/content.component';
+import { TestComponent } from './master/test/test.component';
+import { LoginComponent } from './master/login/login.component';
+import { MasterComponent } from './master/master/master.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    HeaderContentComponent,
+    ContentComponent,
+    TestComponent,
+    LoginComponent,
+    MasterComponent
   ],
   imports: [
     BrowserModule,
